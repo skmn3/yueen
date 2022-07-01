96a4a7a4c53dd6b12de020c4e5cc014463706339
Sangjin <sangjin170202@naver.com>
2022-07-01 13:29:00 +0900

    ⚙ :: 프로젝트 세팅

---
+++ src/main/java/kr/co/yueen/YueenApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yueen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YueenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YueenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/yueen/YueenApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yueen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YueenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
